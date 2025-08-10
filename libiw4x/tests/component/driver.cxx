1220dd704e2ed9a532a3ba95186f211c4859cff4
William Roy <wroy@proton.me>
2025-08-10 12:04:50 -0400

    Test basic_component class

--- libiw4x/tests/component/driver.cxx
+++ libiw4x/tests/component/driver.cxx
@@ -2,11 +2,33 @@
 #include <stdexcept>
 
 #include <libiw4x/version.hxx>
+#include <libiw4x/component.hxx>
 
 #undef NDEBUG
 #include <cassert>
 
+using namespace std;
+
+namespace iw4x
+{
+  class basic_component final : public component<basic_component>
+  {
+  public:
+    basic_component ()
+      : value_ (42) {}
+
+    int
+    get_value () const { return value_; }
+
+    void
+    set_value (int v) { value_ = v; }
+
+  private:
+    int value_ {};
+  };
+}
+
 int main ()
 {
-  using namespace std;
+  using namespace iw4x;
 }
