c80f7a7213dc34d8ef2af8da64ede4f01efb5b42
William Roy <wroy@proton.me>
2025-08-10 11:18:20 -0400

    Encapsulate component type traits

--- libiw4x/libiw4x/component.hxx
+++ libiw4x/libiw4x/component.hxx
@@ -2,11 +2,21 @@
 
 namespace iw4x
 {
+  namespace details
+  {
+    template <typename T>
+    struct component_traits
+    {
+      using type = T;
+    };
+  }
+
   template <typename T>
   class component
   {
   public:
     using component_type = T;
+    using traits_type = details::component_traits<T>;
 
   protected:
     component () = default;
