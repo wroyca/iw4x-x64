1d18bf72eee560cd22c57fd97c15acc3e9e6d173
William Roy <wroy@proton.me>
2025-08-10 12:05:09 -0400

    Draft singleton access method

--- libiw4x/libiw4x/component.hxx
+++ libiw4x/libiw4x/component.hxx
@@ -1,5 +1,9 @@
 #pragma once
 
+#include <type_traits>
+
+#include <libiw4x/export.hxx>
+
 namespace iw4x
 {
   namespace details
@@ -18,7 +22,14 @@ namespace iw4x
     using component_type = T;
     using traits_type = details::component_traits<T>;
 
+    // Get component instance (singleton access).
+    //
+    static T&
+    get ();
+
   protected:
     component () = default;
   };
 }
+
+#include <libiw4x/component.ixx>
---
+++ libiw4x/libiw4x/component.ixx
@@ -0,0 +1,9 @@
+#pragma once
+
+namespace iw4x
+{
+  template <typename T> inline T &component<T>::
+  get ()
+  {
+  }
+}
