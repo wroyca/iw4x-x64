e63c841cdd58b767a1479322e9ddc3f826bda712
William Roy <wroy@proton.me>
2025-08-13 12:05:07 -0400

    Introduce placeholder integration for ImGui frontend

---
+++ libiw4x/libiw4x/imgui.cxx
@@ -0,0 +1,9 @@
+#include <libiw4x/imgui.hxx>
+
+namespace iw4x
+{
+  imgui::imgui ()
+  {
+
+  }
+}
---
+++ libiw4x/libiw4x/imgui.hxx
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <libiw4x/export.hxx>
+
+namespace iw4x
+{
+  class LIBIW4X_SYMEXPORT imgui
+  {
+  public:
+    explicit imgui ();
+  };
+}
