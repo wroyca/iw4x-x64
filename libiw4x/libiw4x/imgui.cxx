7f2a3907b341d8f0467373ee7ca3c6d577291678
William Roy <wroy@proton.me>
2025-10-01 10:44:02 -0400

    Update imgui code style formatting

--- libiw4x/libiw4x/imgui.cxx
+++ libiw4x/libiw4x/imgui.cxx
@@ -23,10 +23,10 @@ ImGui_ImplWin32_WndProcHandler (HWND hWnd,
 
 namespace iw4x
 {
-  // Static member definitions
-  //
   namespace
   {
+    once_flag imgui_init;
+
     WNDPROC original_wnd_proc_ (nullptr);
   }
 
@@ -39,17 +39,16 @@ namespace iw4x
       // and there is no point in doing it twice if the first frame never
       // arrives.
       //
-      static once_flag imgui_init_once_flag; call_once (imgui_init_once_flag,
-                                                        [device] ()
+      call_once (imgui_init, [device] ()
       {
-        HRESULT hr (0);
+        HRESULT hr;
 
         // To integrate ImGui we need the window handle that owns the device.
         // Direct3D does not hand this out directly; the swap chain is the
         // sanctioned path. The detour feels roundabout but avoids maintaining
         // parallel knowledge of windowing state.
         //
-        IDirect3DSwapChain9* swap_chain (nullptr);
+        IDirect3DSwapChain9* swap_chain;
         hr = device->GetSwapChain (0, &swap_chain);
         if (FAILED (hr))
           throw runtime_error ("unable to get DirectX swap chain");
@@ -71,6 +70,7 @@ namespace iw4x
           //
           cerr << "warning: device window could not be determined; "
                << "defaulting to current foreground window" << endl;
+
           hwnd = GetForegroundWindow ();
         }
 
@@ -102,7 +102,8 @@ namespace iw4x
         // Hook window procedure to forward messages to ImGui.
         //
         original_wnd_proc_ = reinterpret_cast<WNDPROC> (
-          SetWindowLongPtrW (hwnd, GWLP_WNDPROC,
+          SetWindowLongPtrW (hwnd,
+                             GWLP_WNDPROC,
                              reinterpret_cast<LONG_PTR> (wnd_proc)));
 
         if (original_wnd_proc_ == nullptr)
@@ -121,7 +122,8 @@ namespace iw4x
     });
   }
 
-  void imgui::render_frame (IDirect3DDevice9* device)
+  void
+  imgui::render_frame (IDirect3DDevice9* device)
   {
     // Start the ImGui frame
     //
@@ -147,18 +149,18 @@ namespace iw4x
     ImGui_ImplDX9_RenderDrawData (ImGui::GetDrawData ());
   }
 
-  LRESULT CALLBACK imgui::wnd_proc (HWND hwnd,
-                                    UINT msg,
-                                    WPARAM wparam,
-                                    LPARAM lparam)
+  LRESULT CALLBACK
+  imgui::wnd_proc (HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam)
   {
     // Forward the message to ImGui first. If ImGui handles it, we're done.
-    // Otherwise, we pass it through to the original window procedure to
-    // maintain normal application behavior.
+    // Otherwise, we pass it through to the original window procedure.
     //
-    LRESULT imgui_result (ImGui_ImplWin32_WndProcHandler (hwnd, msg, wparam, lparam));
-    if (imgui_result != 0)
-      return imgui_result;
+    LRESULT imgui_wnd_proc (ImGui_ImplWin32_WndProcHandler (hwnd,
+                                                            msg,
+                                                            wparam,
+                                                            lparam));
+    if (imgui_wnd_proc != 0)
+      return imgui_wnd_proc;
 
     // Fallback to iw4mp window procedure if ImGui doesn't handle the
     // message.
