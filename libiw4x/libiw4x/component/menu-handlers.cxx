d96a72eee0f021792936114c3b598abd81280990
William Roy <wroy@proton.me>
2025-10-04 15:49:56 -0400

    Sketch menu handlers component

---
+++ libiw4x/libiw4x/component/menu-handlers.cxx
@@ -0,0 +1,9 @@
+#include <libiw4x/component/menu-handlers.hxx>
+
+namespace iw4x
+{
+  menu_handlers::menu_handlers ()
+  {
+
+  }
+}
---
+++ libiw4x/libiw4x/component/menu-handlers.hxx
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <libiw4x/database.hxx>
+
+#include <libiw4x/utility-win32.hxx>
+
+namespace iw4x
+{
+  class menu_handlers
+  {
+  public:
+    menu_handlers ();
+  };
+}
