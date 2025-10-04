5eb00d6d9af094d2bbc05e6c9763291677866608
William Roy <wroy@proton.me>
2025-10-04 16:01:31 -0400

    Introduce lookup helpers for resolving menus and items

--- libiw4x/libiw4x/component/menu-handlers.cxx
+++ libiw4x/libiw4x/component/menu-handlers.cxx
@@ -2,8 +2,30 @@
 
 namespace iw4x
 {
-  menu_handlers::menu_handlers ()
-  {
+  menu_handlers::menu_handlers () = default;
 
+  menuDef_t*
+  menu_handlers::find_menu (const string& n)
+  {
+    XAssetHeader h (DB_FindXAssetHeader (ASSET_TYPE_MENU, n.c_str()));
+
+    return h.menu;
+  }
+
+  itemDef_s*
+  menu_handlers::find_item (menuDef_t* m, const string& name)
+  {
+    for (int i (0); i < m->itemCount; ++i)
+    {
+      itemDef_s* item (m->items [i]);
+
+      // Note that we have no guarantee that the engine will always match item
+      // count with the actual items array size.
+      //
+      if ((item != nullptr) && (item->text != nullptr) && item->text == name)
+        return item;
+    }
+
+    throw runtime_error (format ("unable to resolve menu item: {}", name));
   }
 }
--- libiw4x/libiw4x/component/menu-handlers.hxx
+++ libiw4x/libiw4x/component/menu-handlers.hxx
@@ -1,14 +1,25 @@
 #pragma once
 
 #include <libiw4x/database.hxx>
+#include <libiw4x/symbols.hxx>
 
 #include <libiw4x/utility-win32.hxx>
 
 namespace iw4x
 {
+  inline XAssetEntry* g_assetEntryPool = reinterpret_cast<XAssetEntry*> (0x141567FD0);
+  inline unsigned short* db_hashTable = reinterpret_cast<unsigned short*> (0x141551E90);
+  inline XAssetHeader (*DB_FindXAssetHeader) (XAssetType type, const char* name) = reinterpret_cast<XAssetHeader (*) (XAssetType, const char*)> (0x140129220);
+
   class menu_handlers
   {
   public:
     menu_handlers ();
+
+    menuDef_t*
+    find_menu (const string& name);
+
+    itemDef_s*
+    find_item (menuDef_t*, const string& name);
   };
 }
