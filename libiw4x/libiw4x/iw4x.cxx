ebff7eed837b4e59b5b16d7d6e92c177c0d1bbed
William Roy <wroy@proton.me>
2025-10-14 15:42:53 -0400

    Define library-controlled entry point

--- libiw4x/libiw4x/iw4x.cxx
+++ libiw4x/libiw4x/iw4x.cxx
@@ -1,18 +1,18 @@
 #include <libiw4x/iw4x.hxx>
 
-#include <ostream>
-#include <stdexcept>
-
-using namespace std;
-
 namespace iw4x
 {
-  void
-  say_hello (ostream& o, const string& n)
+  extern "C"
   {
-    if (n.empty ())
-      throw invalid_argument ("empty name");
+    BOOL WINAPI
+    DllMain (HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved)
+    {
+      if (fdwReason != DLL_PROCESS_ATTACH)
+        return TRUE;
 
-    o << "Hello, " << n << '!' << endl;
+      // Successful DLL_PROCESS_ATTACH.
+      //
+      return TRUE;
+    }
   }
 }
