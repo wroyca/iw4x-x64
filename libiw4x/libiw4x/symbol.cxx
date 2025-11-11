1c0a99c5950f106e96521495dcbe06df4017588b
William Roy <wroy@proton.me>
2025-11-11 16:21:45 -0500

    Add engine-symbol registry

---
+++ libiw4x/libiw4x/symbol.cxx
@@ -0,0 +1,7 @@
+#include <libiw4x/symbol.hxx>
+
+namespace iw4x
+{
+  Com_Frame_Try_Block_Function_t Com_Frame_Try_Block_Function =
+    reinterpret_cast<Com_Frame_Try_Block_Function_t> (0x1401F9930);
+}
---
+++ libiw4x/libiw4x/symbol.hxx
@@ -0,0 +1,7 @@
+#pragma once
+
+namespace iw4x
+{
+  using  Com_Frame_Try_Block_Function_t = void (*) ();
+  extern Com_Frame_Try_Block_Function_t Com_Frame_Try_Block_Function;
+}
