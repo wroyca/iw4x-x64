4cac4e0106faeb3d62ce557a75328efca0dae206
William Roy <wroy@proton.me>
2025-11-12 15:00:46 -0500

    Set C++ standard to latest in tests build configurations

--- libiw4x-utility/tests/format-message/driver.cxx
+++ libiw4x-utility/tests/format-message/driver.cxx
@@ -16,21 +16,21 @@ int main ()
     // ERROR_SUCCESS (0)
     //
     {
-      string msg = format_message (0);
+      string msg (format_message (0));
       assert (!msg.empty ());
     }
 
     // ERROR_FILE_NOT_FOUND (2)
     //
     {
-      string msg = format_message (2);
+      string msg (format_message (2));
       assert (!msg.empty ());
     }
 
     // ERROR_ACCESS_DENIED (5)
     //
     {
-      string msg = format_message (5);
+      string msg (format_message (5));
       assert (!msg.empty ());
     }
   }
@@ -38,14 +38,14 @@ int main ()
   // Test that unknown error code return fallback message
   //
   {
-    string msg = format_message (999999999);
+    string msg (format_message (999999999));
     assert (msg == "unknown error code 999999999");
   }
 
   // Test that messages don't contain trailing whitespace/newlines
   //
   {
-    string msg = format_message (2);
+    string msg (format_message (2));
     assert (!msg.empty () && msg.back () != '\n' && msg.back () != '\r');
   }
 }
