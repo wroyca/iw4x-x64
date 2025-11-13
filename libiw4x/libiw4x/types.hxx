49d378778a988483d5a36b5bebb8e6f78460e379
William Roy <wroy@proton.me>
2025-11-12 19:11:36 -0500

    Add initial definitions for network source and address types

---
+++ libiw4x/libiw4x/types.hxx
@@ -0,0 +1,50 @@
+#pragma once
+
+// 29
+//
+enum netsrc_t
+{
+  NS_CLIENT1 = 0x0,
+  NS_SERVER = 0x1,
+  NS_MAXCLIENTS = 0x1,
+  NS_PACKET = 0x2,
+};
+
+// 100
+//
+enum netadrtype_t
+{
+  NA_BOT = 0x0,
+  NA_BAD = 0x1,
+  NA_LOOPBACK = 0x2,
+  NA_BROADCAST = 0x3,
+  NA_IP = 0x4,
+  NA_IPX = 0x5,
+  NA_BROADCAST_IPX = 0x6,
+};
+
+// 1315
+//
+struct netadr_t
+{
+  netadrtype_t type;
+  char ip[4];
+  unsigned short port;
+  char ipx[10];
+};
+
+// 1474
+//
+struct msg_t
+{
+  int overflowed;
+  int readOnly;
+  char *data;
+  char *splitData;
+  int maxsize;
+  int cursize;
+  int splitSize;
+  int readcount;
+  int bit;
+  int lastEntityRef;
+};
