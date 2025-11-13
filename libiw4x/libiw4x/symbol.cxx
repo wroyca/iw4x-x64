2c7ee738a9008c64b765a330d9d72ae757c196aa
William Roy <wroy@proton.me>
2025-11-12 19:11:27 -0500

    Import Dvar and NET functions

--- libiw4x/libiw4x/symbol.cxx
+++ libiw4x/libiw4x/symbol.cxx
@@ -7,4 +7,31 @@ namespace iw4x
 
   DB_FindXAssetHeader_t DB_FindXAssetHeader =
   reinterpret_cast<DB_FindXAssetHeader_t> (0x140129220);
+
+  Dvar_RegisterString_t Dvar_RegisterString =
+  reinterpret_cast<Dvar_RegisterString_t> (0x140288590);
+
+  Dvar_RegisterInt_t Dvar_RegisterInt =
+  reinterpret_cast<Dvar_RegisterInt_t> (0x1402881F0);
+
+  Dvar_FindVar_t Dvar_FindVar =
+  reinterpret_cast<Dvar_FindVar_t> (0x140287170);
+
+  Dvar_SetString_t Dvar_SetString =
+  reinterpret_cast<Dvar_SetString_t> (0x140289A80);
+
+  Dvar_SetFromStringByName_t Dvar_SetFromStringByName =
+  reinterpret_cast<Dvar_SetFromStringByName_t> (0x140289570);
+
+  Dvar_RegisterBool_t Dvar_RegisterBool =
+  reinterpret_cast<Dvar_RegisterBool_t> (0x140287CE0);
+
+  NET_Config_t NET_Config =
+  reinterpret_cast<NET_Config_t> (0x1402A9510);
+
+  NET_GetDvars_t NET_GetDvars =
+  reinterpret_cast<NET_GetDvars_t> (0x1402A9900);
+
+  NET_SendPacket_t NET_SendPacket =
+  reinterpret_cast<NET_SendPacket_t> (0x1402AA1B0);
 }
--- libiw4x/libiw4x/symbol.hxx
+++ libiw4x/libiw4x/symbol.hxx
@@ -9,4 +9,31 @@ namespace iw4x
 
   using  DB_FindXAssetHeader_t = XAssetHeader (*) (XAssetType type, const char* name);
   extern DB_FindXAssetHeader_t DB_FindXAssetHeader;
+
+  using  Dvar_RegisterString_t = __int64 (*) (__int64, __int64, int, __int64);
+  extern Dvar_RegisterString_t Dvar_RegisterString;
+
+  using  Dvar_RegisterInt_t = __int64 (*) (__int64, int, int, int, int, __int64);
+  extern Dvar_RegisterInt_t Dvar_RegisterInt;
+
+  using  Dvar_FindVar_t = dvar_t* (*) (const char* name);
+  extern Dvar_FindVar_t Dvar_FindVar;
+
+  using  Dvar_SetString_t = __int64 (*) (__int64, __int64);
+  extern Dvar_SetString_t Dvar_SetString;
+
+  using  Dvar_SetFromStringByName_t = __int64 (*) (__int64, __int64);
+  extern Dvar_SetFromStringByName_t Dvar_SetFromStringByName;
+
+  using  Dvar_RegisterBool_t = __int64 (*) (__int64, __int64, int, __int64);
+  extern Dvar_RegisterBool_t Dvar_RegisterBool;
+
+  using  NET_Config_t = int (*) (int);
+  extern NET_Config_t NET_Config;
+
+  using  NET_GetDvars_t = bool (*) ();
+  extern NET_GetDvars_t NET_GetDvars;
+
+  using  NET_SendPacket_t = bool (*) (size_t, char*, int*);
+  extern NET_SendPacket_t NET_SendPacket;
 }
