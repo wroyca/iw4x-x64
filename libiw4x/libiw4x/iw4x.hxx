5cbb739ba835ac242e145db6f8f532e46409c8fd
William Roy <wroy@proton.me>
2025-11-15 15:16:35 -0500

    Define library-controlled entry point

--- libiw4x/libiw4x/iw4x.hxx
+++ libiw4x/libiw4x/iw4x.hxx
@@ -1,15 +1,40 @@
 #pragma once
 
-#include <iosfwd>
-#include <string>
+// Include Windows.h in a way that avoids the usual namespace pollution.
+//
+// In particular, we temporarily define WIN32_LEAN_AND_MEAN to exclude rarely
+// used headers, and NOMINMAX to suppress the intrusive min() and max() macros.
+// These macros are problematic because they interfere with standard library
+// functions and templates. Note that we restore the macro state afterward to
+// avoid affecting other translation units.
+//
+#ifndef WIN32_LEAN_AND_MEAN
+#  define WIN32_LEAN_AND_MEAN
+#  ifndef NOMINMAX
+#    define NOMINMAX
+#    include <windows.h>
+#    undef NOMINMAX
+#  else
+#    include <windows.h>
+#  endif
+#  undef WIN32_LEAN_AND_MEAN
+#else
+#  ifndef NOMINMAX
+#    define NOMINMAX
+#    include <windows.h>
+#    undef NOMINMAX
+#  else
+#    include <windows.h>
+#  endif
+#endif
 
 #include <libiw4x/export.hxx>
 
 namespace iw4x
 {
-  // Print a greeting for the specified name into the specified
-  // stream. Throw std::invalid_argument if the name is empty.
-  //
-  LIBIW4X_SYMEXPORT void
-  say_hello (std::ostream&, const std::string& name);
+  extern "C"
+  {
+    BOOL WINAPI
+    DllMain (HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved);
+  }
 }
--- libiw4x/tests/basics/driver.cxx
+++ libiw4x/tests/basics/driver.cxx
@@ -12,25 +12,4 @@ main ()
 {
   using namespace std;
   using namespace iw4x;
-
-  // Basics.
-  //
-  {
-    ostringstream o;
-    say_hello (o, "World");
-    assert (o.str () == "Hello, World!\n");
-  }
-
-  // Empty name.
-  //
-  try
-  {
-    ostringstream o;
-    say_hello (o, "");
-    assert (false);
-  }
-  catch (const invalid_argument& e)
-  {
-    assert (e.what () == string ("empty name"));
-  }
 }
