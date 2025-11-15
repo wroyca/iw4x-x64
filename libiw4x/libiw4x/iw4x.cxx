e3d14120856908c3ded107fe8c555cb3001e602c
William Roy <wroy@proton.me>
2025-11-15 16:03:05 -0500

    Add libiw4x-utility package to existing project

--- libiw4x/libiw4x/iw4x.cxx
+++ libiw4x/libiw4x/iw4x.cxx
@@ -12,6 +12,18 @@ namespace iw4x
       if (fdwReason != DLL_PROCESS_ATTACH)
         return TRUE;
 
+      uintptr_t source (0x140358EBC);
+      uintptr_t target (reinterpret_cast<uintptr_t> (+[] ()
+      {
+        // __security_init_cookie
+        //
+        reinterpret_cast<void (*) ()> (0x1403598CC) ();
+
+        // __scrt_common_main_seh
+        //
+        return reinterpret_cast<int (*) ()> (0x140358D48) ();
+      }));
+
       // Successful DLL_PROCESS_ATTACH.
       //
       return TRUE;
